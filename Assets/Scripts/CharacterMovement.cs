38363e7adedc9a917de4d1609a8d69ba0edb6c51
Connor-Hughes <hughesconnor6r@gmail.com>
2019-10-14 15:48:16 +0200

    Initial commit

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public Camera camera;
+    public bool followCharacter;
+    public AudioClip coinCollect;
+    private bool airborne = false;
+    public float thrust;
+    public float maxVelocity;
+
+    private int direction = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb = GetComponent<Rigidbody2D>();
+            if (Mathf.Abs(rb.velocity.x) < maxVelocity)
+            {
+                rb.AddForce(transform.right * thrust);
+            }
+
+            if (direction == 1)
+            {
+                SpriteRenderer sr = GetComponent<SpriteRenderer>();
+                sr.flipX = false;
+            }
+            direction = 0;
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb = GetComponent<Rigidbody2D>();
+            if (Mathf.Abs(rb.velocity.x) < maxVelocity)
+            {
+                rb.AddForce(transform.right * -thrust);
+            }
+
+            if (direction == 0)
+            {
+                SpriteRenderer sr = GetComponent<SpriteRenderer>();
+                sr.flipX = true;
+            }
+
+            direction = 1;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb = GetComponent<Rigidbody2D>();
+                if (!airborne && Mathf.Abs(rb.velocity.y) < maxVelocity)
+                {
+                    rb.AddForce(transform.up * thrust * 40f);
+                    airborne = true;
+                }
+            }
+
+            if (followCharacter)
+            {
+                Vector3 newPos = new Vector3(this.transform.position.x, this.transform.position.y, -10);
+                camera.transform.position = newPos;
+            }
+
+            void OnCollisionEnter2D(Collision2D coll)
+            {
+                if (coll.gameObject.tag == "platform")
+                {
+                    airborne = false;
+                }
+                else if (coll.gameObject.tag == "ramp")
+                {
+                    rb = GetComponent<Rigidbody2D>();
+                    if (Mathf.Abs(rb.velocity.y) < maxVelocity)
+                        rb.AddForce(transform.up * thrust * 40f);
+                    airborne = true;
+                }
+
+            }
+
+            void OnTriggerEnter2D(Collider2D coll)
+            {
+
+                if (coll.gameObject.tag == "coin")
+                {
+                    AudioSource.PlayClipAtPoint(coinCollect, transform.position);
+                    Destroy(coll.gameObject);
+                }
+            }
+        }
+    }
+}
